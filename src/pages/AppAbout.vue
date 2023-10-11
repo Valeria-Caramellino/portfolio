363a7eaefe996f26cc226bebaa428770a0b9ee35
valeria caramellino <valeriacaramellino@gmail.com>
2023-10-11 15:16:01 +0200

    about page

---
+++ src/pages/AppAbout.vue
@@ -0,0 +1,34 @@
+<script>
+import Navbar from '../components/Navbar.vue';
+import ContainerAbout from '../components/ContainerAbout.vue';
+export default{
+    name:"AppAbout",
+    components:{
+        Navbar,
+        ContainerAbout
+    }
+}
+
+</script>
+<template>
+
+    <body>
+        <header>
+            <Navbar :img='"src/assets/log/logo_bianco.png"'/>
+        </header>
+        <main>
+            <ContainerAbout />
+        </main>
+    </body>
+
+</template>
+<style lang="scss" scoped>
+
+body{
+  height: 100vh;
+  width: 100vw;
+  background-image: url(../assets/img_bg/sfondo_nero.png);
+  background-size: 100% 120% ;
+}
+
+</style>
