582503f9aa8578be7895ab11ce154eee844a4a02
valeria caramellino <valeriacaramellino@gmail.com>
2023-10-11 15:15:50 +0200

    home page

---
+++ src/pages/AppHome.vue
@@ -0,0 +1,44 @@
+<script>
+
+import Navbar from '../components/Navbar.vue';
+import ContainerMain from '../components/ContainerMain.vue';
+
+export default{
+  name:'AppHome',
+  components:{
+     Navbar,
+     ContainerMain
+  },
+  data(){
+    return{
+      
+    }
+  }
+}
+
+</script>
+
+<template>
+  <body>
+    <header>
+      <Navbar :img='"src/assets/log/logo_verde.png"'/>
+    </header>
+    <main>
+      <ContainerMain/>     
+    </main>
+  </body>
+
+  
+</template>
+
+<style lang="scss" scoped>
+
+@use'../style/variable.scss' as*;
+body{
+  height: 100vh;
+  width: 100vw;
+  background-image: url(../assets/img_bg/sfondo_bianco.png);
+  background-size: 100% 120% ;
+}
+
+</style>
