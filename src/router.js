e160e16a1aedecb5990c55c3591ae1fc24bd4970
valeria caramellino <valeriacaramellino@gmail.com>
2023-10-11 15:14:26 +0200

    add router

--- src/router.js
+++ src/router.js
@@ -3,23 +3,28 @@ import { createRouter , createWebHistory } from "vue-router";
 
 //importi le pagine
 // import AppHome from './pages/AppHome.vue';
-
+import AppHome from './pages/AppHome.vue';
+import AppAbout from './pages/AppAbout.vue';
 const router = createRouter({
 
     history: createWebHistory(),
     routes:[
         // crei rotte es 
         //se devi ridinizzonare un percorso metti path: '/' e redirect:'/home'
-        // {
-        //     path: '/',
-        //     redirect: '/home'
-        // },
-        // {
-        //     path: '/home',
-        //     name: 'home',
-        //     component: AppHome
-        // },
-       
+        {
+            path: '/',
+            redirect: '/home'
+        },
+        {
+            path: '/home',
+            name: 'home',
+            component : AppHome
+        },
+        {
+            path:'/ChiSono',
+            name: 'about',
+            component: AppAbout
+        },
 
     ]
 
