afe5fa4d3c014139afa13e0fc6c050b7824de508
valeria caramellino <valeriacaramellino@gmail.com>
2023-10-26 11:06:07 +0200

    edit

--- src/router.js
+++ src/router.js
@@ -1,8 +1,5 @@
 import { createRouter , createWebHistory } from "vue-router";
 
-
-//importi le pagine
-
 import AppHome from './pages/AppHome.vue';
 import AppAbout from './pages/AppAbout.vue';
 import AppSkill from './pages/AppSkill.vue';
@@ -13,8 +10,6 @@ const router = createRouter({
 
     history: createWebHistory(),
     routes:[
-        // crei rotte es 
-        //se devi ridinizzonare un percorso metti path: '/' e redirect:'/home'
         {
             path: '/',
             redirect: '/home'
