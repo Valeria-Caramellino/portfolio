980b6441012a53e11d7d07d8ebd815f42a9abbe2
valeria caramellino <valeriacaramellino@gmail.com>
2023-10-26 17:02:10 +0200

    add store for list projects

---
+++ src/store.js
@@ -0,0 +1,30 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    group_projects: [
+        {
+            id:1,
+            github: "https://github.com/Valeria-Caramellino",
+            img: "/src/assets/img_progetti/whats.png",
+            title: "Descrizione",
+            text: "Whats Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae, quod quidem accusamus adipisci consequatur error, magnam inventore fugiat eum voluptas quibusdam perferendis. Quia fugit odio quidem repellendus obcaecati iusto harum!",
+        },
+        {
+            id:2,
+            github: "https://github.com/Valeria-Caramellino/htmlcss-discord",
+            img: "/src/assets/img_progetti/Discord.png",
+            title: "Descrizione",
+            text: "Discord Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae, quod quidem accusamus adipisci consequatur error, magnam inventore fugiat eum voluptas quibusdam perferendis. Quia fugit odio quidem repellendus obcaecati iusto harum  ipsum dolor sit amet consectetur, adipisicing elit. Molestiae, quod quidem accusamus adipisci consequatur error, magnam inventore fugiat eum voluptas quibusdam perferendis. Quia fugit odio quidem repellendus obcaecati iusto harum!",
+        },
+        {   img: "src/assets/img_progetti/Discord.png" },
+        {   img: "src/assets/img_progetti/Discord.png" },
+        {   img: "src/assets/img_progetti/Discord.png" },
+        {   img: "src/assets/img_progetti/Discord.png" },
+        {   img: "src/assets/img_progetti/Discord.png" },
+        {   img: "src/assets/img_progetti/Discord.png" },
+        {   img: "src/assets/img_progetti/Discord.png" },
+        {   img: "src/assets/img_progetti/Discord.png" },
+        {   img: "src/assets/img_progetti/Discord.png" },
+        {   img: "src/assets/img_progetti/Discord.png" },
+    ]
+})
